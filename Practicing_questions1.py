05133c914892e2a58c3e66ce9384e579a8e49d9e
Muqeemudeen <muqeemudeen25@gmail.com>
2024-11-20 23:30:10 +0530

    ending the day with some practise

--- Practicing_questions1.py
+++ Practicing_questions1.py
@@ -15,22 +15,65 @@ a= a.lower()
 print(f"The string in lowercase is : {a}")
 
 
-b = " Cypher " + a
+b = a
+print(a)
 
 
 
 print(f"The copy of the string a is : {b}")
 
-for i in range(1, len(a),1):
-    print( a[-i] )
+
+print("The Reverse of the string is ")
+reve =a[::-1]
+print(reve)
+   
     
 
 #------- Vowels in a given string 
 
-strin = input("Enter the string to find vowels")
+#strin = input("Enter the string to find vowels")
 
-for i in range(len(strin)):
-    if "aeiouAEIOU" in strin:
-        print(f"This letter is a vowel {strin}")
-    else:
-        print(f"There are no vowels")
+#for j in range(len(strin)):
+   # if "aeiouAEIOU" in strin:
+   #     print(f"This letter is a vowel {j}")
+   # else:
+    #    print(f"There are no vowels")
+
+
+
+#--- arrange the string in lowercase first and upper case second
+
+hehe = "Yoo Brother Im in love"
+hehe2 = hehe.split()
+hehe3 =""
+
+
+
+for i in hehe2:
+    if i.islower():
+        hehe3 = hehe3 +i
+print(hehe3)
+for i in hehe2:
+    if i.isupper():
+        hehe3 = hehe3 +i
+print("Ordering lower case first and then upper case in a string:")
+print(hehe3)
+
+
+
+
+
+# next question !!
+#count all letters digit and special char from a given string 
+#count nos 
+
+
+str = "p@#yn26at^8i5ve"
+
+chr =0
+dig =0
+spchr =0 
+
+for i in str :
+    if i.isdigit():
+        dig =dig +1 
